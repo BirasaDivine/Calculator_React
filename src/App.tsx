f2f6e95b857970a874dad714c14091d3a181c9bc
Birasa Divine <b.divine@alustudent.com>
2025-08-05 19:45:55 +0200

    (calculator):UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const buttonArray=["AC","+/-","%","÷","7","8","9","X","4","5","6","-","1","2","3","+","0",".","="]
 
+  const newArray=buttonArray.map(button =>{
+    return <button onClick ={()=>displaying(button)}className={`rounded-full ${ "0".includes(button)?"col-span-2 w-38 bg-amber-500" : ["AC","+/-","%","-","+","."].includes(button)?"bg-amber-500" : ["7","8","9","6","5","4","3","2","1"].includes(button) ? "bg-purple-300" : "bg-green-300" } border size-15`}>{button}</button>
+  })
+  let [display,useDisplay]=React.useState("")
+   let [firstOperand,usefirstOperand]=React.useState("")
+    let [operand,useOperand]=React.useState("")
+  function displaying(button:string){
+    useDisplay( prevbutton=> prevbutton+button)
+  }
+  function changeFirstOperand(button:string){
+    usefirstOperand(prev=>prev+button)
+  }
+  function changeOperand(button:string){
+    useOperand(button)
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-black">
+        <div className="flex justify-center items-center pt-30">
+          <div className='w-[400px] h-[600px] bg-amber-200 rounded-3xl p-9'>
+            <input value={display} className="bg-white   bg-2 rounded-2xl h-[150px] w-[350px] inset-shadow-black/50 inset-shadow-sm ">
+            </input>
+            <div className="grid  grid-cols-4  gap-5 pt-5">
+              {newArray}
+            </div>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
